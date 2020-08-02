52d3a3f4cfea96c2251bb309ddb6fb0eed9128eb
khou <khou2020@outlook.com>
2020-08-02 05:59:08 -0500

    do not analyze selection when there is only 1 block; expose a dataspace ID for contiguous memory space

--- src/logvol_property.cpp
+++ src/logvol_property.cpp
@@ -1,6 +1,6 @@
 #include "logvol_internal.hpp"
 
-#define NB_PROPERTY_NAME "logvol_nonblocking"
+#define NB_PROPERTY_NAME	"logvol_nonblocking"
 #define BSIZE_PROPERTY_NAME "logvol_nb_buffer_size"
 
 herr_t H5Pset_nonblocking (hid_t plist, H5VL_log_req_type_t nonblocking) {
@@ -38,7 +38,8 @@ herr_t H5Pget_nonblocking (hid_t plist, H5VL_log_req_type_t *nonblocking) {
 	else {
 		pexist = H5Pexist (plist, NB_PROPERTY_NAME);
 		CHECK_ID (pexist)
-		if (pexist) {			err = H5Pget (plist, NB_PROPERTY_NAME, nonblocking);
+		if (pexist) {
+			err = H5Pget (plist, NB_PROPERTY_NAME, nonblocking);
 			CHECK_ERR
 
 		} else {
@@ -66,8 +67,8 @@ herr_t H5Pset_nb_buffer_size (hid_t plist, size_t size) {
 	CHECK_ID (pexist)
 	if (!pexist) {
 		ssize_t infty = LOG_VOL_BSIZE_UNLIMITED;
-		err = H5Pinsert2 (plist, BSIZE_PROPERTY_NAME, sizeof (size_t), &infty, NULL,
-						  NULL, NULL, NULL, NULL, NULL);
+		err = H5Pinsert2 (plist, BSIZE_PROPERTY_NAME, sizeof (size_t), &infty, NULL, NULL, NULL,
+						  NULL, NULL, NULL);
 		CHECK_ERR
 	}
 
@@ -89,7 +90,8 @@ herr_t H5Pget_nb_buffer_size (hid_t plist, ssize_t *size) {
 	else {
 		pexist = H5Pexist (plist, BSIZE_PROPERTY_NAME);
 		CHECK_ID (pexist)
-		if (pexist) {			err = H5Pget (plist, BSIZE_PROPERTY_NAME, size);
+		if (pexist) {
+			err = H5Pget (plist, BSIZE_PROPERTY_NAME, size);
 			CHECK_ERR
 
 		} else {
