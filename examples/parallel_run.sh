ae561e9559233f1ba0ac9bc7c6b3bf07beaa75e5
Kaiyuan Hou <khou2020@outlook.com>
2021-12-11 04:00:57 -0600

    support parallel test with make ptest

---
+++ examples/parallel_run.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Copyright (C) 2021, Northwestern University and Argonne National Laboratory
+# See COPYRIGHT notice in top-level directory.
+#
+
+# Exit immediately if a command exits with a non-zero status.
+set -e
+
+MPIRUN=`echo ${TESTMPIRUN} | ${SED} -e "s/NP/$1/g"`
+# echo "MPIRUN = ${MPIRUN}"
+# echo "check_PROGRAMS=${check_PROGRAMS}"
+
+# export HDF5_VOL_CONNECTOR="LOG under_vol=0;under_info={}" 
+export HDF5_PLUGIN_PATH="${top_builddir}/src/.libs"
+
+for p in ${check_PROGRAMS} ; do
+    ${MPIRUN} ./${p} ${TESTOUTDIR}/${p}.h5
+done
