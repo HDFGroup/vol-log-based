f2f8fca84cce69d7be27b41373b952885cef099c
Kaiyuan Hou <khou2020@outlook.com>
2022-03-13 21:07:01 -0500

    download hdf5 examples on the go

--- examples/hdf5_examples/wrap_runs.sh
+++ examples/hdf5_examples/wrap_runs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2003, Northwestern University and Argonne National Laboratory
+# Copyright (C) 2022, Northwestern University and Argonne National Laboratory
 # See COPYRIGHT notice in top-level directory.
 #
 
@@ -9,11 +9,14 @@ set -e
 
 outfile=`basename $1`
 
-# export HDF5_VOL_CONNECTOR="LOG under_vol=0;under_info={}" 
-# export HDF5_PLUGIN_PATH="../../src/.libs"
-
+# Some files are reused by other tests, so we can't switch VOL
 # ensure these 2 environment variables are not set
-unset HDF5_VOL_CONNECTOR
-unset HDF5_PLUGIN_PATH
+# unset HDF5_VOL_CONNECTOR
+# unset HDF5_PLUGIN_PATH
+
+# ${TESTSEQRUN} ./$1 ${TESTOUTDIR}/$outfile.h5
+
+export HDF5_VOL_CONNECTOR="LOG under_vol=0;under_info={}" 
+export HDF5_PLUGIN_PATH="${top_builddir}/src/.libs"
 
 ${TESTSEQRUN} ./$1 ${TESTOUTDIR}/$outfile.h5
