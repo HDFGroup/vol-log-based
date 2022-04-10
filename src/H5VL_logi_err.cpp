569ca942eb798dd94069ac0726a0061ee6363f4b
Kaiyuan Hou <khou2020@outlook.com>
2022-04-10 04:23:19 -0500

    bugfix: add src/H5VL_logi_err.cpp to git

---
+++ src/H5VL_logi_err.cpp
@@ -0,0 +1,29 @@
+/*
+ *  Copyright (C) 2022, Northwestern University and Argonne National Laboratory
+ *  See COPYRIGHT notice in top-level directory.
+ */
+/* $Id$ */
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+//
+#include <exception>
+#include <string>
+//
+#include <H5VL_logi_err.hpp>
+//
+
+H5VL_logi_exception::H5VL_logi_exception (const char *file,
+                                          int line,
+                                          const char *func,
+                                          const char *msg)
+    : file (file), line (line), func (func), msg (msg), is_hdf5 (false) {}
+
+H5VL_logi_exception::H5VL_logi_exception (
+    const char *file, int line, const char *func, std::string msg, bool is_hdf5)
+    : file (file), line (line), func (func), msg (msg), is_hdf5 (is_hdf5) {}
+
+std::string H5VL_logi_exception::what () {
+    return "In " + func + " at " + file + ":" + std::to_string (line) + ": " + msg;
+}
