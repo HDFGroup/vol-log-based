502951759572f79846dac0f440dc3774a14e51cf
Kaiyuan Hou <khou2020@outlook.com>
2022-11-08 16:30:03 -0600

    update setenv test program

--- tests/dynamic/setenv.cpp
+++ tests/dynamic/setenv.cpp
@@ -6,35 +6,62 @@
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
- * This program creates two files. Before creating the second file, the 
- * environment variable for default VOL is changed to pass_through (built-in 
+ * This program creates a file and then opens it. Before opening the file, the
+ * environment variable for default VOL is changed to pass_through (built-in
  * pass through VOL) by the setenv function.
- * 
+ *
  * HDF5 checks the default environment variable at library initialization time,
- * so changing the environment variables should have no effect on the default 
- * VOL used on the second file. Reset all plugin paths and turn the VOL plugin 
+ * so changing the environment variables should have no effect on the default
+ * VOL used on the second file. Reset all plugin paths and turn the VOL plugin
  * off and on does not make any difference.
+ *
+ * To change the VOL after HDF5 initialization, the user has to specify the VOL
+ * to use in file access property programmatically.
  *
  * The compile and run commands are given below.
  *
- *    % mpicxx -g -o setenv setenv.cpp -I../ -lhdf5 
+ *    % mpicxx -g -o setenv setenv.cpp -lhdf5
  *
  *    % mpiexec -l -n 1 setenv setenv.nc
- *    % *** TESTING CXX    setenv: Changing VOL environment variable ---- pass
+ *    % *** TESTING CXX    setenv: Changing VOL environment variable
+ *    % Envinroment variables for file create:
+ *    % HDF5_VOL_CONNECTOR = 
+ *    % HDF5_PLUGIN_PATH = 
+ *    % VOL used is native
+ *    % Envinroment variables for file open:
+ *    % HDF5_VOL_CONNECTOR = pass_through under_vol=0;under_info={}
+ *    % HDF5_PLUGIN_PATH = 
+ *    % VOL used is native
+ *    % pass
  *
  * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
 
 #include <hdf5.h>
 #include <mpi.h>
-#include <stdio.h>
-#include <stdlib.h>
 
+#include <cassert>
+#include <cstdio>
+#include <cstdlib>
 #include <cstring>
+#include <iostream>
 
-#include "H5VL_log.h"
-#include "testutils.hpp"
+#define SHOW_TEST_INFO(A)                                                                       \
+	{                                                                                           \
+		if (rank == 0) {                                                                        \
+			std::cout << "*** TESTING CXX    " << basename (argv[0]) << ": " << A << std::endl; \
+		}                                                                                       \
+	}
 
-#define N 10
+#define SHOW_TEST_RESULT                                                           \
+	{                                                                              \
+		MPI_Allreduce (MPI_IN_PLACE, &nerrs, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD); \
+		if (rank == 0) {                                                           \
+			if (nerrs)                                                             \
+				std::cout << "fail with " << nerrs << " mismatches." << std::endl; \
+			else                                                                   \
+				std::cout << "pass" << std::endl;                                  \
+		}                                                                          \
+	}
 
 int main (int argc, char **argv) {
 	int err, nerrs = 0;
@@ -42,6 +69,7 @@ int main (int argc, char **argv) {
 	unsigned int nidx;
 	int i;
 	char buf[1024];
+	char *env;
 	const char *file_name;
 	hid_t fid	   = -1;  // File ID
 	hid_t faplid   = -1;
@@ -64,58 +92,84 @@ int main (int argc, char **argv) {
 	}
 	SHOW_TEST_INFO ("Changing VOL environment variable")
 
+	if (rank == 0) {
+		std::cout << "Envinroment variables for file create:" << std::endl;
+
+		env = getenv ("HDF5_VOL_CONNECTOR");
+		if (!env) env = const_cast<char *> ("");
+		std::cout << "HDF5_VOL_CONNECTOR = " << env << std::endl;
+
+		env = getenv ("HDF5_PLUGIN_PATH");
+		if (!env) env = const_cast<char *> ("");
+		std::cout << "HDF5_PLUGIN_PATH = " << env << std::endl;
+	}
+
 	faplid = H5Pcreate (H5P_FILE_ACCESS);
-	CHECK_ERR (faplid)
+	assert (faplid >= 0);
 	err = H5Pset_fapl_mpio (faplid, MPI_COMM_WORLD, MPI_INFO_NULL);
-	CHECK_ERR (err)
+	assert (err >= 0);
 
 	// Create file
 	fid = H5Fcreate (file_name, H5F_ACC_TRUNC, H5P_DEFAULT, faplid);
-	CHECK_ERR (fid)
+	assert (fid >= 0);
 
 	err = H5VLget_connector_name (fid, name1, 64);
-	CHECK_ERR (err)
+	assert (err >= 0);
+	if (rank == 0) { std::cout << "VOL used is " << name1 << std::endl; }
 
 	err = H5Fclose (fid);
-	CHECK_ERR (err)
+	assert (err >= 0);
 	fid = -1;
 
 	// Change env
 	setenv ("HDF5_VOL_CONNECTOR", "pass_through under_vol=0;under_info={}", 1);
 
+	if (rank == 0) {
+		std::cout << "Envinroment variables for file open:" << std::endl;
+
+		env = getenv ("HDF5_VOL_CONNECTOR");
+		assert (env);
+		std::cout << "HDF5_VOL_CONNECTOR = " << env << std::endl;
+
+		env = getenv ("HDF5_PLUGIN_PATH");
+		if (!env) env = const_cast<char *> ("");
+		std::cout << "HDF5_PLUGIN_PATH = " << env << std::endl;
+	}
+
 	// Reset all plugin path
 	err = H5PLsize (&nidx);
-	CHECK_ERR (err)
+	assert (err >= 0);
 	for (i = 0; i < nidx; i++) {
 		err = H5PLget (i, buf, 1024);
-		CHECK_ERR (err)
+		assert (err >= 0);
 		err = H5PLreplace (buf, i);
-		CHECK_ERR (err)
+		assert (err >= 0);
 	}
 
 	// Turn VOL plugin off and on
-	err= H5PLget_loading_state(&nidx);
-	CHECK_ERR(err)
+	err = H5PLget_loading_state (&nidx);
+	assert (err >= 0);
 
-	nidx &=~(H5PL_VOL_PLUGIN);
-	err= H5PLset_loading_state(nidx&(~(H5PL_VOL_PLUGIN)));
-	CHECK_ERR(err)
+	nidx &= ~(H5PL_VOL_PLUGIN);
+	err = H5PLset_loading_state (nidx & (~(H5PL_VOL_PLUGIN)));
+	assert (err >= 0);
 
-	err= H5PLset_loading_state(nidx);
-	CHECK_ERR(err)
+	err = H5PLset_loading_state (nidx);
+	assert (err >= 0);
 
 	// Open file
 	fid = H5Fopen (file_name, H5F_ACC_RDONLY, faplid);
-	CHECK_ERR (fid)
+	assert (fid >= 0);
 
 	err = H5VLget_connector_name (fid, name2, 64);
-	CHECK_ERR (err)
+	assert (err >= 0);
+	if (rank == 0) { std::cout << "VOL used is " << name2 << std::endl; }
 
 	err = H5Fclose (fid);
-	CHECK_ERR (err)
+	assert (err >= 0);
 	fid = -1;
 
-	EXP_VAL (std::string (name1), std::string (name2))
+	assert (std::string (name1) == std::string (name2));
 
 err_out:
 	if (fid >= 0) H5Fclose (fid);
