9c316af6293ff6a1b32f01981db1cc3c9a916fc5
wkliao <wkliao@users.noreply.github.com>
2022-11-09 20:56:22 -0600

    retabe to 4 spaces

--- tests/dynamic/test_env.cpp
+++ tests/dynamic/test_env.cpp
@@ -2,7 +2,6 @@
  *  Copyright (C) 2022, Northwestern University and Argonne National Laboratory
  *  See COPYRIGHT notice in top-level directory.
  */
-/* $Id$ */
 
 /* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
  *
@@ -45,140 +44,138 @@
 #include <cstring>
 #include <iostream>
 
-#define SHOW_TEST_INFO(A)                                                                       \
-	{                                                                                           \
-		if (rank == 0) {                                                                        \
-			std::cout << "*** TESTING CXX    " << basename (argv[0]) << ": " << A << std::endl; \
-		}                                                                                       \
-	}
+#define SHOW_TEST_INFO(A) {                                                                 \
+    if (rank == 0) {                                                                        \
+        std::cout << "*** TESTING CXX    " << basename (argv[0]) << ": " << A << std::endl; \
+    }                                                                                       \
+}
 
-#define SHOW_TEST_RESULT                                                           \
-	{                                                                              \
-		MPI_Allreduce (MPI_IN_PLACE, &nerrs, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD); \
-		if (rank == 0) {                                                           \
-			if (nerrs)                                                             \
-				std::cout << "fail with " << nerrs << " mismatches." << std::endl; \
-			else                                                                   \
-				std::cout << "pass" << std::endl;                                  \
-		}                                                                          \
-	}
+#define SHOW_TEST_RESULT {                                                     \
+    MPI_Allreduce (MPI_IN_PLACE, &nerrs, 1, MPI_INT, MPI_SUM, MPI_COMM_WORLD); \
+    if (rank == 0) {                                                           \
+        if (nerrs)                                                             \
+            std::cout << "fail with " << nerrs << " mismatches." << std::endl; \
+        else                                                                   \
+            std::cout << "pass" << std::endl;                                  \
+    }                                                                          \
+}
 
 int main (int argc, char **argv) {
-	int err, nerrs = 0;
-	int rank, np;
-	unsigned int nidx;
-	int i;
-	char buf[1024];
-	char *env;
-	const char *file_name;
-	hid_t fid	   = -1;  // File ID
-	hid_t faplid   = -1;
-	hid_t log_vlid = -1;  // Logvol ID
-	char name1[64];
-	char name2[64];
+    int err, nerrs = 0;
+    int rank, np;
+    unsigned int nidx;
+    int i;
+    char buf[1024];
+    char *env;
+    const char *file_name;
+    hid_t fid       = -1;  // File ID
+    hid_t faplid   = -1;
+    hid_t log_vlid = -1;  // Logvol ID
+    char name1[64];
+    char name2[64];
 
-	MPI_Init (&argc, &argv);
-	MPI_Comm_size (MPI_COMM_WORLD, &np);
-	MPI_Comm_rank (MPI_COMM_WORLD, &rank);
+    MPI_Init (&argc, &argv);
+    MPI_Comm_size (MPI_COMM_WORLD, &np);
+    MPI_Comm_rank (MPI_COMM_WORLD, &rank);
 
-	if (argc > 2) {
-		if (!rank) printf ("Usage: %s [filename]\n", argv[0]);
-		MPI_Finalize ();
-		return 1;
-	} else if (argc > 1) {
-		file_name = argv[1];
-	} else {
-		file_name = "test.h5";
-	}
-	SHOW_TEST_INFO ("Changing VOL environment variable")
+    if (argc > 2) {
+        if (!rank) printf ("Usage: %s [filename]\n", argv[0]);
+        MPI_Finalize ();
+        return 1;
+    } else if (argc > 1) {
+        file_name = argv[1];
+    } else {
+        file_name = "test.h5";
+    }
+    SHOW_TEST_INFO ("Changing VOL environment variable")
 
-	if (rank == 0) {
-		std::cout << "Envinroment variables for file create:" << std::endl;
+    if (rank == 0) {
+        std::cout << "Envinroment variables for file create:" << std::endl;
 
-		env = getenv ("HDF5_VOL_CONNECTOR");
-		if (!env) env = const_cast<char *> ("");
-		std::cout << "HDF5_VOL_CONNECTOR = " << env << std::endl;
+        env = getenv ("HDF5_VOL_CONNECTOR");
+        if (!env) env = const_cast<char *> ("");
+        std::cout << "HDF5_VOL_CONNECTOR = " << env << std::endl;
 
-		env = getenv ("HDF5_PLUGIN_PATH");
-		if (!env) env = const_cast<char *> ("");
-		std::cout << "HDF5_PLUGIN_PATH = " << env << std::endl;
-	}
+        env = getenv ("HDF5_PLUGIN_PATH");
+        if (!env) env = const_cast<char *> ("");
+        std::cout << "HDF5_PLUGIN_PATH = " << env << std::endl;
+    }
 
-	faplid = H5Pcreate (H5P_FILE_ACCESS);
-	assert (faplid >= 0);
-	err = H5Pset_fapl_mpio (faplid, MPI_COMM_WORLD, MPI_INFO_NULL);
-	assert (err >= 0);
+    faplid = H5Pcreate (H5P_FILE_ACCESS);
+    assert (faplid >= 0);
+    err = H5Pset_fapl_mpio (faplid, MPI_COMM_WORLD, MPI_INFO_NULL);
+    assert (err >= 0);
 
-	// Create file
-	fid = H5Fcreate (file_name, H5F_ACC_TRUNC, H5P_DEFAULT, faplid);
-	assert (fid >= 0);
+    // Create file
+    fid = H5Fcreate (file_name, H5F_ACC_TRUNC, H5P_DEFAULT, faplid);
+    assert (fid >= 0);
 
-	err = H5VLget_connector_name (fid, name1, 64);
-	assert (err >= 0);
-	if (rank == 0) { std::cout << "VOL used is " << name1 << std::endl; }
+    err = H5VLget_connector_name (fid, name1, 64);
+    assert (err >= 0);
+    if (rank == 0) { std::cout << "VOL used is " << name1 << std::endl; }
 
-	err = H5Fclose (fid);
-	assert (err >= 0);
-	fid = -1;
+    err = H5Fclose (fid);
+    assert (err >= 0);
+    fid = -1;
 
-	// Change env
-	setenv ("HDF5_VOL_CONNECTOR", "pass_through under_vol=0;under_info={}", 1);
+    // Change env
+    setenv ("HDF5_VOL_CONNECTOR", "pass_through under_vol=0;under_info={}", 1);
 
-	if (rank == 0) {
-		std::cout << "Envinroment variables for file open:" << std::endl;
+    if (rank == 0) {
+        std::cout << "Envinroment variables for file open:" << std::endl;
 
-		env = getenv ("HDF5_VOL_CONNECTOR");
-		assert (env);
-		std::cout << "HDF5_VOL_CONNECTOR = " << env << std::endl;
+        env = getenv ("HDF5_VOL_CONNECTOR");
+        assert (env);
+        std::cout << "HDF5_VOL_CONNECTOR = " << env << std::endl;
 
-		env = getenv ("HDF5_PLUGIN_PATH");
-		if (!env) env = const_cast<char *> ("");
-		std::cout << "HDF5_PLUGIN_PATH = " << env << std::endl;
-	}
+        env = getenv ("HDF5_PLUGIN_PATH");
+        if (!env) env = const_cast<char *> ("");
+        std::cout << "HDF5_PLUGIN_PATH = " << env << std::endl;
+    }
 
-	// Reset all plugin path
-	err = H5PLsize (&nidx);
-	assert (err >= 0);
-	for (i = 0; i < nidx; i++) {
-		err = H5PLget (i, buf, 1024);
-		assert (err >= 0);
-		err = H5PLreplace (buf, i);
-		assert (err >= 0);
-	}
+    // Reset all plugin path
+    err = H5PLsize (&nidx);
+    assert (err >= 0);
+    for (i = 0; i < nidx; i++) {
+        err = H5PLget (i, buf, 1024);
+        assert (err >= 0);
+        err = H5PLreplace (buf, i);
+        assert (err >= 0);
+    }
 
-	// Turn VOL plugin off and on
-	err = H5PLget_loading_state (&nidx);
-	assert (err >= 0);
+    // Turn VOL plugin off and on
+    err = H5PLget_loading_state (&nidx);
+    assert (err >= 0);
 
-	nidx &= ~(H5PL_VOL_PLUGIN);
-	err = H5PLset_loading_state (nidx & (~(H5PL_VOL_PLUGIN)));
-	assert (err >= 0);
+    nidx &= ~(H5PL_VOL_PLUGIN);
+    err = H5PLset_loading_state (nidx & (~(H5PL_VOL_PLUGIN)));
+    assert (err >= 0);
 
-	err = H5PLset_loading_state (nidx);
-	assert (err >= 0);
+    err = H5PLset_loading_state (nidx);
+    assert (err >= 0);
 
-	// Open file
-	fid = H5Fopen (file_name, H5F_ACC_RDONLY, faplid);
-	assert (fid >= 0);
+    // Open file
+    fid = H5Fopen (file_name, H5F_ACC_RDONLY, faplid);
+    assert (fid >= 0);
 
-	err = H5VLget_connector_name (fid, name2, 64);
-	assert (err >= 0);
-	if (rank == 0) { std::cout << "VOL used is " << name2 << std::endl; }
+    err = H5VLget_connector_name (fid, name2, 64);
+    assert (err >= 0);
+    if (rank == 0) { std::cout << "VOL used is " << name2 << std::endl; }
 
-	err = H5Fclose (fid);
-	assert (err >= 0);
-	fid = -1;
+    err = H5Fclose (fid);
+    assert (err >= 0);
+    fid = -1;
 
-	assert (std::string (name1) == std::string (name2));
+    assert (std::string (name1) == std::string (name2));
 
 err_out:
-	if (fid >= 0) H5Fclose (fid);
-	if (faplid >= 0) H5Pclose (faplid);
-	if (log_vlid >= 00) H5VLclose (log_vlid);
+    if (fid >= 0) H5Fclose (fid);
+    if (faplid >= 0) H5Pclose (faplid);
+    if (log_vlid >= 00) H5VLclose (log_vlid);
 
-	SHOW_TEST_RESULT
+    SHOW_TEST_RESULT
 
-	MPI_Finalize ();
+    MPI_Finalize ();
 
-	return (nerrs > 0);
+    return (nerrs > 0);
 }
