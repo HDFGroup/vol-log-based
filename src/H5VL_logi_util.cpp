705e6b006d9ea957e7ebf6e535bfaf4a98859d92
Kaiyuan Hou <khou2020@outlook.com>
2022-11-17 23:25:25 -0600

    fix git H5VL_logi_util

--- src/H5VL_logi_util.cpp
+++ src/H5VL_logi_util.cpp
@@ -47,6 +47,13 @@ void H5VL_logi_add_att (void *uo,
     err = H5VLattr_close (ap, uvlid, dxpl_id, NULL);
     CHECK_ERR
     H5Sclose (asid);
+}
+void H5VL_logi_add_att (H5VL_log_obj_t *op,
+                        const char *name,
+                        hid_t atype,
+                        hid_t mtype,
+                        hsize_t len,
+                        void *buf,
                         hid_t dxpl_id,
                         void **req) {
     H5VL_logi_add_att (op->uo, op->uvlid, op->type, name, atype, mtype, len, buf, dxpl_id, req);
@@ -116,7 +123,6 @@ hbool_t H5VL_logi_exists_att (H5VL_log_obj_t *op, const char *name, hid_t dxpl_i
     attr_check_exists.args.exists.exists = &exist;
     attr_check_exists.op_type            = H5VL_ATTR_EXISTS;
     H5VLattr_specific (op->uo, &loc, op->uvlid, &attr_check_exists, dxpl_id, NULL);
-
     return exist;
 }
 // This methods checks if a link (group or dataset) exists in file
@@ -124,7 +130,6 @@ hbool_t H5VL_logi_exists_link (H5VL_log_file_t *fp, const char *name, hid_t dxpl
     H5VL_link_specific_args_t arg;
     hbool_t exists = 0;
     H5VL_loc_params_t loc;
-
     arg.op_type            = H5VL_LINK_EXISTS;
     arg.args.exists.exists = &exists;
 
@@ -134,7 +139,6 @@ hbool_t H5VL_logi_exists_link (H5VL_log_file_t *fp, const char *name, hid_t dxpl
     loc.loc_data.loc_by_name.lapl_id = H5P_DEFAULT;
 
     H5VLlink_specific (fp->uo, &loc, fp->uvlid, &arg, fp->dxplid, NULL);
-
     return exists;
 }
 
