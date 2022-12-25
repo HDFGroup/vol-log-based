0f9342a33c3fca4a50701deef9578fbe8d10034a
wkliao <wkliao@users.noreply.github.com>
2022-12-25 16:47:30 -0600

    add common files

--- tests/common/wrap_runs.sh
+++ tests/common/wrap_runs.sh
@@ -1,57 +1,66 @@
 #!/bin/sh
 #
-# Copyright (C) 2003, Northwestern University and Argonne National Laboratory
+# Copyright (C) 2022, Northwestern University and Argonne National Laboratory
 # See COPYRIGHT notice in top-level directory.
 #
 
-if test "x${HDF5_VOL_CONNECTOR}" != x ; then
-   async_vol=`echo "${HDF5_VOL_CONNECTOR}" | ${EGREP} -- "under_vol=512"`
-   if test "x$?" = x0 ; then
-      async_vol=yes
+test_func() {
+   set +e
+   log_vol=no
+   cache_vol=no
+   async_vol=no
+   if test "x${HDF5_VOL_CONNECTOR}" != x ; then
+      err=`echo "${HDF5_VOL_CONNECTOR}" | ${EGREP} -- "under_vol=512"`
+      if test "x$?" = x0 ; then
+         async_vol=yes
+      fi
+      err=`echo "${HDF5_VOL_CONNECTOR}" | ${EGREP} -- "under_vol=513"`
+      if test "x$?" = x0 ; then
+         cache_vol=yes
+      fi
+      err=`echo "${HDF5_VOL_CONNECTOR}" | ${EGREP} -- "LOG "`
+      if test "x$?" = x0 ; then
+         log_vol=yes
+      fi
    fi
-   cache_vol=`echo "${HDF5_VOL_CONNECTOR}" | ${EGREP} -- "under_vol=513"`
-   if test "x$?" = x0 ; then
-      cache_vol=yes
+   # echo "log_vol=$log_vol cache_vol=$cache_vol async_vol=$async_vol"
+
+   test_case="terminal passthru"
+   # make Log VOL a passthrough when either cache or async VOL is set
+   if test "x${cache_vol}" = xyes || test "x${async_vol}" = xyes ; then
+      test_case="passthru"
    fi
-   log_vol=`echo "${HDF5_VOL_CONNECTOR}" | ${EGREP} -- "LOG "`
-   if test "x$?" = x0 ; then
-      log_vol=yes
-   fi
-fi
+   # echo "test_case=$test_case"
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
-err=0
-for vol_type in "terminal" "passthru"; do
-   outfile=`basename $1`
-
-   export HDF5_VOL_CONNECTOR="LOG under_vol=0;under_info={}" 
-   export HDF5_PLUGIN_PATH="${top_builddir}/src/.libs"
-   if test "x${vol_type}" = xterminal ; then
-      outfile="${TESTOUTDIR}/$outfile.h5"
-      unset H5VL_LOG_PASSTHRU
-   else
-      outfile="${TESTOUTDIR}/$outfile-passthru.h5"
-      export H5VL_LOG_PASSTHRU=1
-   fi
-
-   ${TESTSEQRUN} ./$1 $outfile
-
-   unset HDF5_VOL_CONNECTOR
-   unset HDF5_PLUGIN_PATH
-   unset H5VL_LOG_PASSTHRU
-
-   if test "x${log_vol}" != xyes ; then
-      continue
+   outfile_kind=HDF5-LogVOL
+   if test "x$2" = x || test "x$2" = x0 ; then
+      if test "x${log_vol}" = xno ; then
+         outfile_kind=HDF5
+      fi
    fi
 
-   FILE_KIND=`${top_builddir}/utils/h5ldump/h5ldump -k $outfile`
-   if test "x${FILE_KIND}" != xHDF5-LogVOL ; then
-      echo "Error: (as $vol_type vol) Output file $outfile is not Log VOL, but ${FILE_KIND}"
-      err=1
-   else
-      echo "Success: (as $vol_type vol) Output file $outfile is ${FILE_KIND}"
-   fi
-done
-exit $err
+   # Exit immediately if a command exits with a non-zero status.
+   set -e
+
+   err=0
+   for vol_type in ${test_case} ; do
+      # echo "vol_type=$vol_type"
+
+      if test "x${vol_type}" = xterminal ; then
+         outfile="${TESTOUTDIR}/$1.h5"
+         unset H5VL_LOG_PASSTHRU
+      else
+         outfile="${TESTOUTDIR}/$1-passthru.h5"
+         export H5VL_LOG_PASSTHRU=1
+      fi
+
+      ${TESTSEQRUN} ./$1 $outfile
+
+      FILE_KIND=`${top_builddir}/utils/h5ldump/h5ldump -k $outfile`
+      if test "x${FILE_KIND}" != x$outfile_kind ; then
+         echo "Error (as $vol_type VOL): expecting file kind $outfile_kind but got ${FILE_KIND}"
+         exit 1
+      fi
+   done
+}
+
