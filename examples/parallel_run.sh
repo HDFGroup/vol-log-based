a33d8bd07b57e1c945c808f59bda68d509e28f51
wkliao <wkliao@users.noreply.github.com>
2022-12-26 04:19:09 -0600

    use functions for repeated tasks

--- examples/parallel_run.sh
+++ examples/parallel_run.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #
-# Copyright (C) 2021, Northwestern University and Argonne National Laboratory
+# Copyright (C) 2022, Northwestern University and Argonne National Laboratory
 # See COPYRIGHT notice in top-level directory.
 #
 
@@ -8,52 +8,11 @@ MPIRUN=`echo ${TESTMPIRUN} | ${SED} -e "s/NP/$1/g"`
 # echo "MPIRUN = ${MPIRUN}"
 # echo "check_PROGRAMS=${check_PROGRAMS}"
 
-# export HDF5_VOL_CONNECTOR="LOG under_vol=0;under_info={}" 
-# export HDF5_PLUGIN_PATH="${top_builddir}/src/.libs"
+source $srcdir/../tests/common/wrap_runs.sh
 
-if test "x${HDF5_VOL_CONNECTOR}" != x ; then
-   async_vol=`echo "${HDF5_VOL_CONNECTOR}" | ${EGREP} -- "under_vol=512"`
-   if test "x$?" = x0 ; then
-      async_vol=yes
-   fi
-   cache_vol=`echo "${HDF5_VOL_CONNECTOR}" | ${EGREP} -- "under_vol=513"`
-   if test "x$?" = x0 ; then
-      cache_vol=yes
-   fi
-   log_vol=`echo "${HDF5_VOL_CONNECTOR}" | ${EGREP} -- "LOG "`
-   if test "x$?" = x0 ; then
-      log_vol=yes
-   fi
-fi
+log_vol_file_only=1
 
-# Exit immediately if a command exits with a non-zero status.
-set -e
-
-err=0
 for p in ${check_PROGRAMS} ; do
-   for vol_type in "terminal" "passthru"; do
-      if test "x${vol_type}" = xterminal ; then
-         if test "x${cache_vol}" = xyes || test "x${async_vol}" = xyes ; then
-            continue
-         fi
-         unset H5VL_LOG_PASSTHRU
-      else
-         export H5VL_LOG_PASSTHRU=1
-      fi
-
-      outfile="${TESTOUTDIR}/${p}.h5"
-
-      ${MPIRUN} ./${p} ${outfile}
-
-      if test "x${log_vol}" != xyes ; then
-         continue
-      fi
-
-      FILE_KIND=`${top_builddir}/utils/h5ldump/h5ldump -k $outfile`
-      if test "x${FILE_KIND}" != xHDF5-LogVOL ; then
-         echo "Error: Output file $outfile is not Log VOL, but ${FILE_KIND}"
-         err=1
-      fi
-   done
+   test_func $p
 done
-exit $err
+
--- tests/basic/seq_runs.sh
+++ tests/basic/seq_runs.sh
@@ -8,11 +8,5 @@ source $srcdir/../common/wrap_runs.sh
 
 log_vol_file_only=1
 
-export HDF5_VOL_CONNECTOR="LOG under_vol=0;under_info={}"
-export HDF5_PLUGIN_PATH="${top_builddir}/src/.libs"
-test_func $1 $log_vol_file_only
-
-unset HDF5_VOL_CONNECTOR
-unset HDF5_PLUGIN_PATH
-test_func $1 $log_vol_file_only
+test_func $1
 
