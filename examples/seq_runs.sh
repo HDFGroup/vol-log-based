b8bc798e68c8e3dda18f1be0c19fe4e914b2c4e7
wkliao <wkliao@users.noreply.github.com>
2022-12-26 14:59:20 -0600

    run examples before examples/hdf5_examples

---
+++ examples/seq_runs.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -l
+#
+# Copyright (C) 2022, Northwestern University and Argonne National Laboratory
+# See COPYRIGHT notice in top-level directory.
+#
+
+. $srcdir/../tests/common/wrap_runs.sh
+
+log_vol_file_only=1
+
+test_func $1
+
