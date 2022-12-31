5dfe37a1903c14a918f8241bba96e6be52d07edb
wkliao <wkliao@users.noreply.github.com>
2022-12-30 19:15:18 -0600

    Skip 'multi_open' due to a bug in Cache VOL
    See https://github.com/hpc-io/vol-cache/pull/17

--- tests/testcases/parallel_run.sh
+++ tests/testcases/parallel_run.sh
@@ -10,9 +10,23 @@ RUN_CMD=`echo ${TESTMPIRUN} | ${SED} -e "s/NP/$1/g"`
 
 . $srcdir/../common/wrap_runs.sh
 
+async_vol=no
+cache_vol=no
+log_vol=no
+getenv_vol
+
 log_vol_file_only=1
 
 for p in ${check_PROGRAMS} ; do
+   exe_base=`basename $p`
+   if test "x$cache_vol" = xyes && test $exe_base = multi_open ; then
+      fmt="####### Skipping %-23s ####################################\n"
+      printf "${fmt}" $p
+      echo "    Skip multi_open due to a bug in Cache VOL"
+      echo "    See https://github.com/hpc-io/vol-cache/pull/17"
+      echo ""
+      continue
+   fi
    test_func $p
 done
 
--- tests/testcases/seq_runs.sh
+++ tests/testcases/seq_runs.sh
@@ -8,6 +8,21 @@ RUN_CMD=${TESTSEQRUN}
 
 . $srcdir/../common/wrap_runs.sh
 
+async_vol=no
+cache_vol=no
+log_vol=no
+getenv_vol
+
+exe_base=`basename $1`
+if test "x$cache_vol" = xyes && test $exe_base = multi_open ; then
+   fmt="####### Skipping %-23s ####################################\n"
+   printf "${fmt}" $1
+   echo "    Skip 'multi_open' due to a bug in Cache VOL"
+   echo "    See https://github.com/hpc-io/vol-cache/pull/17"
+   echo ""
+   exit 0
+fi
+
 log_vol_file_only=1
 
 test_func $1
