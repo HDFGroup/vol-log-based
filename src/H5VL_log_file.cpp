16396dd6ec5569286c6fe73f588ff36c22baf947
zanhua huang <yzanhua@gmail.com>
2023-01-17 13:16:10 -0600

    Flush should flush both read and write requests

--- src/H5VL_log_file.cpp
+++ src/H5VL_log_file.cpp
@@ -447,7 +447,7 @@ herr_t H5VL_log_file_specific (void *file,
                     // Reset hdf5 context to allow dataset operations within a file operation
                     H5VL_logi_reset_lib_stat (lib_state);
 
-                    H5VL_log_nb_flush_write_reqs (fp);
+                    H5VL_log_filei_flush(fp, dxpl_id);
                 } else {
                     err = H5VLfile_specific (fp->uo, fp->uvlid, args, dxpl_id, req);
                 }
